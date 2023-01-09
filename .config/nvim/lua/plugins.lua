e2a415c5fde7cd87f1b77c7621651cdb0b7a63ae
Michael Rutter <1534676+m-rutter@users.noreply.github.com>
2023-01-09 23:12:41 +0000

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,88 +1,128 @@
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    -- TPope Collection
-    use 'tpope/vim-sensible'
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-surround'
-    use 'tpope/vim-sleuth'
-    use 'tpope/vim-markdown'
-    use 'tpope/vim-repeat'
+  use 'editorconfig/editorconfig-vim'
 
-    -- Git
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-rhubarb'
-    
-    -- Icons
-    use 'kyazdani42/nvim-web-devicons'
+  -- Motion plugins
+  use 'justinmk/vim-sneak' -- s-<char>-<char>
 
-    -- Theme
-    use 'glepnir/zephyr-nvim'
+  use 'tpope/vim-commentary' -- comment out stuff
 
-    -- Syntax Highlighting
-    use { 
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+  -- Git
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-rhubarb'
 
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'nvim-lua/lsp_extensions.nvim'
-    use {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("trouble").setup {
-          -- your configuration comes here
-          -- or leave it empty to use the default settings
-          -- refer to the configuration section below
-        }
-      end
-    }
+  -- Themes
+  use 'RRethy/nvim-base16'
+  
+  -- Syntax Highlighting
+  use { 
+	'nvim-treesitter/nvim-treesitter',
+	run = ':TSUpdate'
+  }
 
-    -- UI
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use {'edluffy/specs.nvim'}
+  -- Themes
+  use 'shaunsingh/nord.nvim'
 
-    use {
-    "folke/which-key.nvim",
+  -- GUI
+  use 'itchyny/lightline.vim'
+  use {
+    'andymass/vim-matchup',
+    setup = function()
+      -- may set any options here
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end
+  }
+
+  -- Icons
+  use 'kyazdani42/nvim-web-devicons'
+
+
+  -- Fuzzy Finder
+  use 'airblade/vim-rooter'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Semantic language support
+  use 'neovim/nvim-lspconfig'
+  use 'simrat39/rust-tools.nvim'
+  use 'nvim-lua/lsp_extensions.nvim'
+  use 'ray-x/lsp_signature.nvim'
+  use {
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
     config = function()
-      require("which-key").setup()
-      end
-    }
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
 
-    
-    -- Navigation
-    use 'preservim/nerdtree'
-    use {
-    'nvim-telescope/telescope.nvim',
-     requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use 'justinmk/vim-sneak'
+  -- Debugging
+  use 'nvim-lua/plenary.nvim'
+  use 'mfussenegger/nvim-dap'
 
-    -- Languages 
-    use 'cespare/vim-toml'
+  -- Completion framework:
+  use 'hrsh7th/nvim-cmp' 
+
+  -- LSP completion source:
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Useful completion sources:
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'                             
+  use 'hrsh7th/cmp-path'                              
+  use 'hrsh7th/cmp-buffer'                            
+  use 'hrsh7th/vim-vsnip'      
+
 end)
-
 
 -- Setup Tree Sitter
 require('nvim-treesitter.configs').setup {
-  ensure_installed = "maintained",
+  ensure_installed = {
+	  "rust", 
+	  "typescript", 
+	  "javascript", 
+	  "python", 
+	  "json", 
+	  "yaml", 
+	  "html", 
+    "lua",
+	  "css", 
+	  "dockerfile", 
+	  "diff", 
+	  "cmake", 
+	  "gitcommit", 
+	  "graphql", 
+	  "make", 
+    "markdown",
+	  "racket", 
+	  "regex", 
+	  "java", 
+	  "scala", 
+	  "scss", 
+	  "terraform", 
+	  "toml", 
+	  "tsx",
+  },
+  auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = {'org'},
   },
   indent = {
     enable = true
+  },
+   rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
   }
 }
-
--- Setup airline theme
-require('lualine').setup()
